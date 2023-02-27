241ca27ed9dafd44a5cdef6cb9dc998859abec89
jukry <94563540+jukry@users.noreply.github.com>
2023-02-27 15:56:43 +0200

    styling and changes of RecipeDetails, rendering of ingredient amount and ingredient

--- client/src/Components/RecipeDetails.jsx
+++ client/src/Components/RecipeDetails.jsx
@@ -6,19 +6,32 @@ export default function RecipeDetails() {
     const { id } = useParams()
     const { name, description, instructions, ingredients, images } =
         recipes.find((obj) => obj.id === parseInt(id))
+
     return (
-        <section>
+        <section className="recipe-wrapper">
             <div className="recipe-hero">
                 <img src={`../${images}`} alt="img" />
-            </div>
-            <div>
                 <h1>{name}</h1>
-                <h2>{description}</h2>
-                <h3>
-                    {instructions.map((item, i) => {
-                        return <li key={i}>{item}</li>
+                <h3>{description}</h3>
+            </div>
+            <div className="recipe-data">
+                <div className="instructions">
+                    <ol>
+                        {instructions.map((item, i) => {
+                            return <li key={i}>{item}</li>
+                        })}
+                    </ol>
+                </div>
+                <div className="ingredients">
+                    {ingredients.map(([amount, ingredient], i) => {
+                        return (
+                            <div className="ingr-line">
+                                <p>{amount}</p>
+                                <p>{ingredient}</p>
+                            </div>
+                        )
                     })}
-                </h3>
+                </div>
             </div>
         </section>
     )
