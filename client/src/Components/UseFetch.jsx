e2520bfda37cf8bedb1dfb9c7ded2e8eee68fb51
jukry <94563540+jukry@users.noreply.github.com>
2023-03-16 14:53:50 +0200

    added loading state

--- client/src/Components/UseFetch.jsx
+++ client/src/Components/UseFetch.jsx
@@ -2,15 +2,19 @@ import { useEffect, useState } from "react"
 
 export default function UseFetch(url) {
     const [backendData, setBackendData] = useState(null)
-
+    const [loading, setLoading] = useState(false)
     useEffect(() => {
+        setLoading(true)
         async function getData(url) {
             const response = await fetch(url)
             const responseBody = await response.json()
             const message = responseBody.message
             setBackendData(message)
+            setLoading(false)
         }
         getData(url)
     }, [])
-    return backendData
+    if (!loading) {
+        return backendData
+    }
 }
