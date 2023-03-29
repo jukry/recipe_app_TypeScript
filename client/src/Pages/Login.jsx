ae4fa148e197bed4d349838dcbc2f648081b7030
jukry <94563540+jukry@users.noreply.github.com>
2023-03-29 16:50:49 +0300

    Login form inputs and styling

--- client/src/Pages/Login.jsx
+++ client/src/Pages/Login.jsx
@@ -1,5 +1,34 @@
 import React from "react"
+import "./styles/Login.css"
+
+function handleChange() {
+    //
+}
+function handleSubmit() {
+    //
+}
 
 export default function Login() {
-    return <div>Login</div>
+    return (
+        <div className="login-container">
+            <h2>Kirjaudu</h2>
+            <form onSubmit={handleSubmit} className="login-form">
+                <input
+                    type="email"
+                    name="email"
+                    onChange={handleChange}
+                    placeholder="Sähköpostiosoite"
+                    value=""
+                />
+                <input
+                    type="password"
+                    name="password"
+                    onChange={handleChange}
+                    placeholder="Salasana"
+                    value=""
+                />
+                <button>Kirjaudu sisään</button>
+            </form>
+        </div>
+    )
 }
