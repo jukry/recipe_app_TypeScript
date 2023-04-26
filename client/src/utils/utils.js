cb0e36adea6c303495b80df5de4221ca3d67b30b
jukry <94563540+jukry@users.noreply.github.com>
2023-04-26 11:45:08 +0300

    added loginUser and requireAuth utility functions

--- client/src/utils/utils.js
+++ client/src/utils/utils.js
@@ -1,3 +1,4 @@
+import { redirect } from "react-router-dom"
 import UseFetch from "../Components/UseFetch"
 
 export async function getRecipes(url) {
@@ -7,4 +8,28 @@ export async function getRecipes(url) {
 export async function getRecipeById(url) {
     const data = UseFetch(url)
     return data
+}
+export async function loginUser(creds) {
+    /* const res = await fetch("/api/login", {
+        method: "post",
+        body: JSON.stringify(creds),
+    })
+    const data = await res.json()
+
+    if (!res.ok) {
+        throw {
+            message: data.message,
+            statusText: res.statusText,
+            status: res.status,
+        }
+    }
+
+    return data */
+    return true
+}
+export async function requireAuth() {
+    const loggedIn = JSON.parse(localStorage.getItem("loggedIn"))
+    if (!loggedIn) {
+        throw redirect("/login?message=Kirjaudu ensin sisään")
+    }
 }
