92102b276c15920dee52e9784814342d38c42211
jukry <94563540+jukry@users.noreply.github.com>
2023-04-26 09:01:53 +0300

    added mobile menu with a hamburger icon to open and X to close

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -1,5 +1,7 @@
+import { useState } from "react"
 import { NavLink, Link } from "react-router-dom"
 export default function Navbar() {
+    const [showNav, setShowNav] = useState(false)
     return (
         <header>
             <Link className="logo-container" to="/">
@@ -7,11 +9,34 @@ export default function Navbar() {
                     My <span>Recipes</span>
                 </h1>
             </Link>
-            <nav className="top-nav">
-                <NavLink to="/">Reseptit</NavLink>
-                <NavLink to="account">Oma tili</NavLink>
-                <NavLink to="login">Kirjaudu</NavLink>
+            <nav className="nav-container">
+                <section
+                    className={`top-nav ${
+                        showNav ? "top-nav-active" : "top-nav-disabled"
+                    }`}
+                >
+                    <NavLink to="/">Reseptit</NavLink>
+                    <NavLink to="account">Oma tili</NavLink>
+                    <NavLink to="login">Kirjaudu</NavLink>
+                </section>
             </nav>
+
+            <div
+                onClick={() => setShowNav((prev) => !prev)}
+                className="nav-button"
+            >
+                <span
+                    className={`${!showNav ? "line line-1" : "cross cross-1"}`}
+                ></span>
+                <span
+                    className={`${!showNav ? "line line-2" : "cross cross-2"}`}
+                ></span>
+                <span
+                    className={`${
+                        !showNav ? "line line-3" : `style="display:none;"`
+                    }`}
+                ></span>
+            </div>
         </header>
     )
 }
