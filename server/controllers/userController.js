e25d1f4504281bd940233394a85da8201ba162e9
jukry <94563540+jukry@users.noreply.github.com>
2023-05-04 16:48:46 +0300

    Refactored reg and auth to include token. generateToken function

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -16,14 +16,19 @@ const registerUser = async (req, res) => {
 
     const hashedPassword = await bcrypt.hash(password, saltRounds)
 
-    const user = User.create({
+    const user = await User.create({
         username: username,
         email: email,
         password: hashedPassword,
     })
 
     user
-        ? res.status(200).json({ message: "User created" })
+        ? res.status(201).json({
+              _id: user.id,
+              username: user.username,
+              email: user.email,
+              token: generateToken(user._id),
+          })
         : res
               .status(400)
               .json({ Message: "Something went wrong, please try again" })
@@ -31,14 +36,27 @@ const registerUser = async (req, res) => {
 const authenticateUser = async (req, res) => {
     const { email, password } = req.body
     const user = await User.findOne({ email: email })
+
     if (user && bcrypt.compareSync(password, user.password)) {
-        return res.status(200).json({ Message: "Login succesful" })
+        return res.status(200).json({
+            _id: user.id,
+            username: user.username,
+            email: user.email,
+            token: generateToken(user._id),
+        })
     } else {
         return res.status(404).json({ Message: "Login not succesful" })
     }
 }
 const getUserData = async (req, res) => {
     return res.json({ Message: "Get user data" })
+}
+
+// Generate token
+const generateToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: "7d",
+    })
 }
 
 export { registerUser, authenticateUser, getUserData }
