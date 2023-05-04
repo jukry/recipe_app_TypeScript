3f229826eec6e8872b42f7d1e513cbda87f0d87c
jukry <94563540+jukry@users.noreply.github.com>
2023-05-04 16:49:06 +0300

    Created middleware to check for token match

---
+++ server/middleware/loginMiddleware.js
@@ -0,0 +1,31 @@
+import jwt from "jsonwebtoken"
+import User from "../models/User.js"
+
+const protect = async (req, res, next) => {
+    let token
+
+    if (
+        req.headers.authorization &&
+        req.headers.authorization.startsWith("Bearer")
+    ) {
+        try {
+            // get token from header
+            token = req.headers.authorization.split(" ")[1]
+            // verify
+            const decoded = jwt.verify(token, process.env.JWT_SECRET)
+
+            //get user from token
+            req.user = await User.findById(decoded.id).select("-password")
+
+            next()
+        } catch (error) {
+            console.log(error)
+            res.status(401).json({ Message: "Not authorized" })
+        }
+    }
+    if (!token) {
+        res.status(401).json({ Message: "No token" })
+    }
+}
+
+export default protect
