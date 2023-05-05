b91dabdedb0f7d86ac571ff5dce908fdfe21306e
jukry <94563540+jukry@users.noreply.github.com>
2023-05-05 13:51:00 +0300

    Added return status. Removed console.log

--- server/middleware/loginMiddleware.js
+++ server/middleware/loginMiddleware.js
@@ -19,8 +19,7 @@ const protect = async (req, res, next) => {
 
             next()
         } catch (error) {
-            console.log(error)
-            res.status(401).json({ Message: "Not authorized" })
+            return res.status(401).json({ Message: "Not authorized" })
         }
     }
     if (!token) {
