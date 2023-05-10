174d846161ae11f4fd7d4a3208b0098254d99777
jukry <94563540+jukry@users.noreply.github.com>
2023-05-10 13:19:03 +0300

    Username context displayed

--- client/src/Pages/Account/AccountDashboard.jsx
+++ client/src/Pages/Account/AccountDashboard.jsx
@@ -1,16 +1,21 @@
-import React from "react"
+import React, { useContext } from "react"
 import "./styles/account.css"
-import { requireAuth } from "../../utils/utils"
+import AuthContext from "../../Components/AuthContext"
 
-export async function loader({ request }) {
-    await requireAuth({ request })
+export async function loader() {
+    /* const data = await fetch("http://localhost:5000/users/user", {
+        method: "GET",
+        credentials: "include",
+    }) */
+    //console.log(data.json())
     return null
 }
 
 export default function AccountDashboard() {
+    const { username, email } = useContext(AuthContext)
     return (
         <section className="account-details">
-            <h3>Omat tiedot</h3>
+            <h3>{username}</h3>
         </section>
     )
 }
