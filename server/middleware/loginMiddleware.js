475149c6715b7de6e4e3557da36f6a38a2259f99
jukry <94563540+jukry@users.noreply.github.com>
2023-05-10 13:23:47 +0300

    Get token from request

--- server/middleware/loginMiddleware.js
+++ server/middleware/loginMiddleware.js
@@ -2,26 +2,19 @@ import jwt from "jsonwebtoken"
 import User from "../models/User.js"
 
 const protect = async (req, res, next) => {
-    let token
+    const token = req.cookies.token
 
-    if (
-        req.headers.authorization &&
-        req.headers.authorization.startsWith("Bearer")
-    ) {
-        try {
-            // get token from header
-            token = req.headers.authorization.split(" ")[1]
-            // verify
-            const decoded = jwt.verify(token, process.env.JWT_SECRET)
+    try {
+        // verify
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
 
-            //get user from token
-            req.user = await User.findById(decoded.id).select("-password")
-
-            next()
-        } catch (error) {
-            return res.status(401).json({ Message: "Not authorized" })
-        }
+        //get user from token
+        req.user = await User.findById(decoded.id).select("-password")
+        next()
+    } catch (error) {
+        return res.status(401).json({ Message: "Not authorized" })
     }
+
     if (!token) {
         res.status(401).json({ Message: "No token" })
     }
