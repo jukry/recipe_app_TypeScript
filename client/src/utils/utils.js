9f8038f5da64d3ffa67f8acf4090e246dd19d257
jukry <94563540+jukry@users.noreply.github.com>
2023-05-16 15:45:55 +0300

    getUserData function

--- client/src/utils/utils.js
+++ client/src/utils/utils.js
@@ -1,5 +1,5 @@
 import { redirect } from "react-router-dom"
-import UseFetch from "../Components/UseFetch"
+import UseFetch from "../Hooks/UseFetch"
 
 export async function getRecipes(url) {
     const data = UseFetch(url)
@@ -9,22 +9,22 @@ export async function getRecipeById(url) {
     const data = UseFetch(url)
     return data
 }
-/* export async function requireAuth({ request }) {
-    const pathname = new URL(request.url).pathname
-    //const loggedIn = JSON.parse(localStorage.getItem("loggedIn"))
-    if (!token) {
-        throw redirect(
-            `/login?message=Kirjaudu ensin sisään&redirectTo=${pathname}`
-        )
-    }
-} */
 
-export async function getUserData() {
-    console.log("Get user data")
-    const userData = await fetch("http://localhost:5000/users/user", {
-        method: "GET",
-        credentials: "include",
-    })
-    const body = await userData.json()
-    return body
+export async function getUserData({ request }) {
+    const pathname = new URL(request.url).pathname
+    try {
+        const data = await fetch("http://localhost:5000/users/user", {
+            method: "get",
+            credentials: "include",
+        })
+        const body = await data.json()
+        if (!body.id) {
+            return redirect(
+                `/login?message=Kirjaudu ensin sisään&redirectTo=${pathname}`
+            )
+        }
+        return body
+    } catch (error) {
+        throw new Error(error)
+    }
 }
