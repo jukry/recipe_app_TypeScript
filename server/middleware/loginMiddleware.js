ff62f89c1fe722938d2aa92cd965ca8544ba59b2
jukry <94563540+jukry@users.noreply.github.com>
2023-05-16 15:48:22 +0300

    refactor of protect function to get token from cookies

--- server/middleware/loginMiddleware.js
+++ server/middleware/loginMiddleware.js
@@ -1,18 +1,46 @@
 import jwt from "jsonwebtoken"
 import User from "../models/User.js"
+import { refresh } from "../controllers/authController.js"
 
 const protect = async (req, res, next) => {
-    const token = req.cookies.token
+    if (!req.headers.cookie)
+        return res.status(401).json({ Message: "No token" })
+    const cookies = req.headers.cookie?.split("; ")
+    const token = cookies[0]?.split("=")[1]
+    let acc = cookies[1]?.split("=")[1]
+
+    //set if access token has expired
+    if (token && !acc) {
+        const response = await fetch("http://localhost:5000/auth/refresh", {
+            method: "GET",
+            mode: "cors",
+            credentials: "include",
+            headers: {
+                "Content-Type": "application/json",
+                Cookie: token,
+            },
+        })
+        acc =
+            response?.headers
+                ?.get("set-cookie")
+                ?.split("=")[1]
+                .split("; ")[0] || ""
+        res.cookie("acc", acc, {
+            maxAge: 60 * 10000, // 10 minutes
+            //secure: false, in production, use true
+            httpOnly: true,
+        })
+    }
 
     try {
         // verify
-        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        const decoded = jwt.verify(acc, process.env.JWT_SECRET)
 
         //get user from token
         req.user = await User.findById(decoded.id).select("-password")
         next()
     } catch (error) {
-        return res.status(401).json({ Message: "Not authorized" })
+        return res.status(401).json({ Message: "Invalid token" })
     }
 
     if (!token) {
