fa70d760ca1ee8f7e0140e5b62e206fe312107e3
jukry <94563540+jukry@users.noreply.github.com>
2023-05-16 15:23:52 +0300

    Added context provider wrapping

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,5 +2,10 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App"
 import "./index.css"
+import { AuthContextProvider } from "./Components/AuthContext"
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />)
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <AuthContextProvider>
+        <App />
+    </AuthContextProvider>
+)
