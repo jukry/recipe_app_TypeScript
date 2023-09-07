4c8d85ab1a59793371b29ab340fda6fac3c87bb8
jukry <94563540+jukry@users.noreply.github.com>
2023-09-07 10:04:47 +0300

    favorite recipes fetching and rendering

--- client/src/Pages/Account/FavoriteRecipes.jsx
+++ client/src/Pages/Account/FavoriteRecipes.jsx
@@ -1,7 +1,12 @@
-import React from "react"
+import React, { useContext } from "react"
 import "./styles/account.css"
 import { getUserData } from "../../utils/utils"
 import { redirect } from "react-router-dom"
+import { UserContext } from "../../Context/UserContext"
+import { useQuery } from "@tanstack/react-query"
+import fetchRecipes from "../../Hooks/fetchRecipes"
+import Fooditem from "../../Components/Fooditem"
+import "./styles/favRecipes.css"
 
 export async function loader({ request }) {
     const res = await getUserData({ request })
@@ -12,9 +17,19 @@ export async function loader({ request }) {
 }
 
 export default function FavoriteRecipes() {
-    return (
-        <section className="favrecipes-container">
-            <div>Suosikkireseptit</div>
-        </section>
-    )
+    const queryResponse = useQuery(["recipes"], fetchRecipes)
+    const favRecipes = queryResponse?.data?.message ?? []
+    const context = useContext(UserContext)
+    const contextFavRecipes = context.user.favrecipes
+
+    function favRecipesMap() {
+        return favRecipes.map((item) => {
+            if (contextFavRecipes?.includes(item.id)) {
+                return <Fooditem props={[item]} key={item.id} />
+            }
+        })
+    }
+    const userFavRecipes = favRecipesMap()
+
+    return <section className="favrecipes-container">{userFavRecipes}</section>
 }
