02ba7de1f544d9dda6572dadd2d7bb758fccacb2
jukry <94563540+jukry@users.noreply.github.com>
2023-09-07 10:06:25 +0300

    routes for favorite recipe requests

--- server/routes/user.js
+++ server/routes/user.js
@@ -7,10 +7,16 @@ import {
     getUserRecipes,
 } from "../controllers/userController.js"
 import protect from "../middleware/loginMiddleware.js"
+import {
+    addFavRecipe,
+    deleteFavRecipe,
+} from "../controllers/favRecipeController.js"
 
 userRouter.post("/", registerUser)
 userRouter.post("/login", authenticateUser)
 userRouter.get("/user", protect, getUserData)
 userRouter.get("/user/recipes", protect, getUserRecipes)
+userRouter.post("/user/favrecipes", protect, addFavRecipe)
+userRouter.delete("/user/favrecipes", protect, deleteFavRecipe)
 
 export default userRouter
