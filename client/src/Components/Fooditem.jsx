0745014a20423d7f3e503791cfbe3fe07570722b
jukry <94563540+jukry@users.noreply.github.com>
2023-09-07 10:01:35 +0300

    favorite click handling and heart

--- client/src/Components/Fooditem.jsx
+++ client/src/Components/Fooditem.jsx
@@ -1,8 +1,63 @@
+import { useContext } from "react"
 import { Link } from "react-router-dom"
+import { UserContext } from "../Context/UserContext"
 
 export default function Fooditem(props) {
     const data = props.props[0]
     const searchParams = props.props[1] ?? ""
+    const { user, setUser } = useContext(UserContext)
+    const { favrecipes } = user
+
+    async function handleFavorite(event) {
+        const isfav = event.target.id
+        if (isfav === "isfav") {
+            async function deleteFav() {
+                return await fetch(
+                    import.meta.env.VITE_USERFAVRECIPES_ENDPOINT,
+                    {
+                        method: "DELETE",
+                        mode: "cors",
+                        headers: {
+                            "Content-Type": "application/json",
+                        },
+                        credentials: "include",
+                        body: JSON.stringify({ id: data.id || data._id }),
+                    }
+                )
+            }
+
+            const body = await deleteFav()
+            const message = await body.json()
+            const newFavRecipes = message.Message
+            setUser((prev) => ({
+                ...prev,
+                favrecipes: newFavRecipes,
+            }))
+        } else {
+            async function addFav() {
+                return await fetch(
+                    import.meta.env.VITE_USERFAVRECIPES_ENDPOINT,
+                    {
+                        method: "POST",
+                        mode: "cors",
+                        headers: {
+                            "Content-Type": "application/json",
+                        },
+                        credentials: "include",
+                        body: JSON.stringify({ id: data.id || data._id }),
+                    }
+                )
+            }
+
+            const body = await addFav()
+            const message = await body.json()
+            const newFavRecipes = message.Message
+            setUser((prev) => ({
+                ...prev,
+                favrecipes: newFavRecipes,
+            }))
+        }
+    }
 
     function recipesFunc() {
         return (
@@ -27,6 +82,24 @@ export default function Fooditem(props) {
                 <div className="recipe-info">
                     <h2>{data.name}</h2>
                     <p>{data.description}</p>
+                    <button
+                        className="fav-heart"
+                        onClick={(event) => {
+                            event.stopPropagation()
+                            event.preventDefault()
+                            handleFavorite(event, data._id)
+                        }}
+                    >
+                        {favrecipes?.includes(data.id || data._id) ? (
+                            <p id="isfav" key={data.id || data._id}>
+                                &#x2665;
+                            </p>
+                        ) : (
+                            <p id="notfav" key={data.id}>
+                                &#x2661;
+                            </p>
+                        )}
+                    </button>
                 </div>
             </Link>
         )
