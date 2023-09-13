5cf0a28b9fb6d04b027743df233d53e460fa8a19
jukry <94563540+jukry@users.noreply.github.com>
2023-09-13 15:46:28 +0300

    feat: conditional rendering for content container

--- client/src/Layouts/HomeLayout.jsx
+++ client/src/Layouts/HomeLayout.jsx
@@ -1,16 +1,26 @@
-import React from "react"
+import React, { useContext } from "react"
 import { Outlet } from "react-router-dom"
 import Footer from "../Components/Footer"
 import Navbar from "../Components/Navbar"
+import { UserContext } from "../Context/UserContext"
+import Loader from "../Components/Loader"
 
 export default function HomeLayout() {
+    const { isLoading } = useContext(UserContext)
+    console.log(isLoading)
     return (
         <div className="container">
-            <Navbar />
-            <main>
-                <Outlet />
-            </main>
-            <Footer />
+            {isLoading ? (
+                <Loader />
+            ) : (
+                <>
+                    <Navbar />
+                    <main>
+                        <Outlet />
+                    </main>
+                    <Footer />
+                </>
+            )}
         </div>
     )
 }
