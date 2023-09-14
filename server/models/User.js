45dd3e2e490aff75d170b1d2e8616b64dfc8d800
jukry <94563540+jukry@users.noreply.github.com>
2023-09-14 11:26:28 +0300

    chore: removed username from user model

--- server/models/User.js
+++ server/models/User.js
@@ -2,10 +2,6 @@ import { Schema, model } from "mongoose"
 
 const user = new Schema(
     {
-        username: {
-            type: String,
-            required: [true, "Please add a name"],
-        },
         email: {
             type: String,
             required: [true, "Please add an email"],
