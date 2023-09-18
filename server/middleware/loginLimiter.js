b7c156ab3c40c96d9d86aa5e04ddf68fdc70dade
jukry <94563540+jukry@users.noreply.github.com>
2023-09-18 14:22:05 +0300

    fix: loginLimiter time multiplier

--- server/middleware/loginLimiter.js
+++ server/middleware/loginLimiter.js
@@ -1,11 +1,9 @@
 import { rateLimit } from "express-rate-limit"
-
 const loginLimiter = rateLimit({
-    windowMs: 60 * 100,
+    windowMs: 60 * 1000,
     max: 20,
-    message: {
-        message: "Too many login attempts",
-    },
+    message: "Too many login attempts",
+
     handler: (req, res, next, options) => {
         res.status(options.statusCode).send(options.message)
         next()
