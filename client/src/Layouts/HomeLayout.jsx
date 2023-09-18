f85e63253f2090c1b09040698ea69b657c0af926
jukry <94563540+jukry@users.noreply.github.com>
2023-09-18 12:08:57 +0300

    feat: passing props to navbar; click handling for clicking anywhere on page

--- client/src/Layouts/HomeLayout.jsx
+++ client/src/Layouts/HomeLayout.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react"
+import React, { useContext, useState } from "react"
 import { Outlet } from "react-router-dom"
 import Footer from "../Components/Footer"
 import Navbar from "../Components/Navbar"
@@ -7,13 +7,31 @@ import Loader from "../Components/Loader"
 
 export default function HomeLayout() {
     const { isLoading } = useContext(UserContext)
+    const [showNav, setShowNav] = useState(false)
+
+    function handleNavClick(e) {
+        if (showNav) {
+            setShowNav(() => {
+                return false
+            })
+        } else if (
+            !showNav &&
+            (e.target.className === "nav-button" ||
+                e.target.className.includes("line"))
+        ) {
+            setShowNav(() => {
+                return true
+            })
+        }
+    }
+
     return (
-        <div className="container">
+        <div className="container" onClick={handleNavClick}>
             {isLoading ? (
                 <Loader />
             ) : (
                 <>
-                    <Navbar />
+                    <Navbar props={showNav} handleNavClick={handleNavClick} />
                     <main>
                         <Outlet />
                     </main>
