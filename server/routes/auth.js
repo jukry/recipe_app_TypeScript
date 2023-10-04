7b5895b091c1d2d6b46ffc73e44a95c97ef860e5
jukry <94563540+jukry@users.noreply.github.com>
2023-10-04 13:54:15 +0300

    hotfix: removed loginLimiter

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,7 +3,7 @@ const authRouter = express.Router()
 import { login, logout, refresh } from "../controllers/authController.js"
 import loginLimiter from "../middleware/loginLimiter.js"
 
-authRouter.post("/", loginLimiter, login)
+authRouter.post("/", login)
 authRouter.get("/refresh", refresh)
 authRouter.post("/logout", logout)
 
