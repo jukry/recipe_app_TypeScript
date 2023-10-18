5568befd69792daaa42c850e08bc19aee562175b
jukry <94563540+jukry@users.noreply.github.com>
2023-10-18 15:14:26 +0300

    fefactor: using mutation to handle comment submit

--- client/src/Components/CommentForm.jsx
+++ client/src/Components/CommentForm.jsx
@@ -1,20 +1,55 @@
-import React, { useState } from "react"
+import React, { useContext, useState } from "react"
 import "./Styles/recipeComments.css"
+import { useParams } from "react-router-dom"
+import { UserContext } from "../Context/UserContext"
+import { useMutation } from "@tanstack/react-query"
 
-function CommentForm() {
-    const [comment, setComment] = useState("")
-
+function CommentForm(props) {
+    const { id } = useParams()
+    const { user } = useContext(UserContext)
+    const [comment, setComment] = useState({
+        content: "",
+        username: "",
+    })
+    const handleSubmit = props.props
     const handleCommentChange = (e) => {
-        setComment(e.target.value)
+        setComment((prev) => ({
+            ...prev,
+            content: e.target.value,
+        }))
     }
-    return (
+    const handleCommentUsername = (e) => {
+        setComment((prev) => ({
+            ...prev,
+            username: e.target.value,
+        }))
+    }
+
+    const { mutate, isLoading } = useMutation({
+        mutationFn: handleSubmit,
+    })
+
+    return !user.id ? (
+        <p>Kirjaudu sisään jos haluat kommentoida reseptiä</p> //TODO disable submit button if not logged in?
+    ) : (
         <section id="comment-form-container">
-            {comment ? (
-                <p id="comment-length-field">{comment.length}/1000 merkkiä</p>
+            {comment?.content?.length > 0 ? (
+                <p id="comment-length-field">
+                    {comment?.content?.length}/1000 merkkiä
+                </p>
             ) : (
                 ""
             )}
-            <form action="post" id="comment-form">
+            <form
+                method="post"
+                action=""
+                id="comment-form"
+                onSubmit={(event) => {
+                    event.preventDefault()
+                    mutate([event, comment, id, user.id, setComment])
+                }}
+                disabled={isLoading}
+            >
                 <label htmlFor="comment-box"></label>
                 <textarea
                     name="commentbox"
@@ -24,10 +59,21 @@ function CommentForm() {
                     form="comment-form"
                     placeholder="Kirjoita kommenttisi"
                     maxLength={1000}
-                    value={comment}
+                    value={comment.content}
                     onChange={handleCommentChange}
                     required
                 ></textarea>
+                <label htmlFor="comment-username"></label>
+                <input
+                    id="comment-username"
+                    type="text"
+                    className="text"
+                    placeholder="Anna nimi"
+                    value={comment.username}
+                    onChange={handleCommentUsername}
+                    required
+                    maxLength={64}
+                />
                 <button type="submit" id="comment-submit-button">
                     Lisää kommentti
                 </button>
