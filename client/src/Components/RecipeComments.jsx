ff6808a5f017ccc31c5169bccdf35f9adcac1d2b
jukry <94563540+jukry@users.noreply.github.com>
2023-10-18 15:16:10 +0300

    refactor: prop drilling

--- client/src/Components/RecipeComments.jsx
+++ client/src/Components/RecipeComments.jsx
@@ -2,10 +2,12 @@ import React from "react"
 import CommentForm from "./CommentForm"
 import Comments from "./Comments"
 
-function RecipeComments({ comments }) {
+function RecipeComments(props) {
+    const comments = props.props[0]
+    const handleSubmit = props.props[1]
     return (
         <section id="comment-section">
-            <CommentForm />
+            <CommentForm props={handleSubmit} />
             <Comments comments={comments} />
         </section>
     )
