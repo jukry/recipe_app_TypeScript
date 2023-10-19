c207cb7d7ee7d21ba8565932129ded6da569a1bd
jukry <94563540+jukry@users.noreply.github.com>
2023-10-19 16:27:03 +0300

    refactor: timeout uses useEffect

--- client/src/Components/Loader.jsx
+++ client/src/Components/Loader.jsx
@@ -1,14 +1,18 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import "./Styles/loader.css"
 
 export default function ({ text }) {
     const [message, setMessage] = useState(text)
-    setTimeout(() => {
-        setMessage("Odota hetki, palvelin on idle-tilassa")
-    }, 5000)
+    //this is just used to show user a message if server is in idle. used only with a free deployed server that that goes to idle mode if there are no connections in 15 minutes
+    useEffect(() => {
+        setTimeout(() => {
+            setMessage("Odota hetki, palvelin taitaa olla idle-tilassa")
+        }, 5000)
+    }, [])
+
     return (
         <section id="loader">
-            <h3 id={"loader-text"}>{message}</h3>
+            <h3 id="loader-text">{message}</h3>
             <div id="loading-bars">
                 <div id="bar1" className="loading-bar"></div>
                 <div id="bar2" className="loading-bar"></div>
