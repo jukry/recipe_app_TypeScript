dd568a2491b1178be63b83b34229a0beb3e86b5e
jukry <94563540+jukry@users.noreply.github.com>
2023-10-19 11:38:58 +0300

    refactor: added state and timeout to handle long loading time

--- client/src/Components/Loader.jsx
+++ client/src/Components/Loader.jsx
@@ -1,10 +1,14 @@
-import React from "react"
+import React, { useState } from "react"
 import "./Styles/loader.css"
 
 export default function ({ text }) {
+    const [message, setMessage] = useState(text)
+    setTimeout(() => {
+        setMessage("Odota hetki, palvelin on idle-tilassa")
+    }, 5000)
     return (
         <section id="loader">
-            <h3 id={"loader-text"}>{text}</h3>
+            <h3 id={"loader-text"}>{message}</h3>
             <div id="loading-bars">
                 <div id="bar1" className="loading-bar"></div>
                 <div id="bar2" className="loading-bar"></div>
