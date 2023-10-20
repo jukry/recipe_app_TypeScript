976a5562984f5a1dc516652165b39598d56df736
jukry <94563540+jukry@users.noreply.github.com>
2023-10-20 17:58:09 +0300

    refactor: using logout dispatch; mobile nav moved to component

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -1,11 +1,15 @@
-import { useContext, useEffect, useState } from "react"
 import { NavLink, Link } from "react-router-dom"
+import ShowMobileNavButton from "./ShowMobileNavButton"
+import { useLogout } from "../Hooks/useLogout"
+import { useContext } from "react"
 import { UserContext } from "../Context/UserContext"
 
 export default function Navbar(props) {
-    const { isLoggedIn, setIsLoggedIn } = useContext(UserContext)
+    const { isLoggedIn } = useContext(UserContext)
     const showNav = props.props[0]
     const showNavBar = props.props[1]
+    const { logout } = useLogout()
+
     return (
         <header className={!showNavBar ? "header-hidden" : "header-show"}>
             <Link className="logo-container" to="/">
@@ -21,51 +25,24 @@ export default function Navbar(props) {
                 >
                     <NavLink to="/">Reseptit</NavLink>
                     <NavLink to="account">Oma tili</NavLink>
-                    {isLoggedIn ? (
-                        <NavLink
-                            to="/"
-                            onClick={async () => {
-                                setIsLoggedIn((prev) => !prev)
-                                await fetch(
-                                    process.env.NODE_ENV === "production"
-                                        ? import.meta.env.VITE_LOGOUT_ENDPOINT
-                                        : import.meta.env
-                                              .VITE_LOGOUT_ENDPOINT_DEV,
-                                    {
-                                        method: "POST",
-                                        credentials: "include",
-                                    }
-                                )
-                                location.replace("/")
-                            }}
-                        >
+                    {!isLoggedIn ? (
+                        <NavLink to="login">Kirjaudu sisään</NavLink>
+                    ) : (
+                        <NavLink to="/" onClick={async () => await logout()}>
                             Kirjaudu ulos
                         </NavLink>
-                    ) : (
-                        <NavLink to="login">Kirjaudu sisään</NavLink>
                     )}
-                    {isLoggedIn ? (
-                        ""
-                    ) : (
+                    {!isLoggedIn ? (
                         <NavLink to="/register">Rekisteröidy</NavLink>
+                    ) : (
+                        ""
                     )}
                 </section>
             </nav>
-
-            <div
-                onClick={(e) => props.handleNavClick(e)}
-                className="nav-button"
-            >
-                <span
-                    className={`${!showNav ? "line line-1" : "cross cross-1"}`}
-                ></span>
-                <span
-                    className={`${!showNav ? "line line-2" : "cross cross-2"}`}
-                ></span>
-                <span
-                    className={`${!showNav ? "line line-3" : "line-hidden"}`}
-                ></span>
-            </div>
+            <ShowMobileNavButton
+                showNav={showNav}
+                handleNavClick={props.handleNavClick}
+            />
         </header>
     )
 }
