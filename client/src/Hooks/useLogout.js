cb9f0ebadc8f194c4b42f2b0bfe2eb986e3079d6
jukry <94563540+jukry@users.noreply.github.com>
2023-10-20 17:40:39 +0300

    feat: useLogout hook

---
+++ client/src/Hooks/useLogout.js
@@ -0,0 +1,24 @@
+import { useContext } from "react"
+import { UserContext } from "../Context/UserContext"
+
+export const useLogout = () => {
+    const { dispatch } = useContext(UserContext)
+    const { setIsLoggedIn } = useContext(UserContext)
+
+    const logout = async () => {
+        const res = await fetch(
+            process.env.NODE_ENV === "production"
+                ? import.meta.env.VITE_LOGOUT_ENDPOINT
+                : import.meta.env.VITE_LOGOUT_ENDPOINT_DEV,
+            {
+                method: "POST",
+                credentials: "include",
+            }
+        )
+        if (res.ok) {
+            dispatch({ type: "LOGOUT" })
+            setIsLoggedIn(false)
+        }
+    }
+    return { logout }
+}
