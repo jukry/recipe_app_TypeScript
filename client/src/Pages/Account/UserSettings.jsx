5446fe2de5c9e2455622129932bb4f445d566370
jukry <94563540+jukry@users.noreply.github.com>
2023-10-22 20:30:42 +0300

    feat: component render

--- client/src/Pages/Account/UserSettings.jsx
+++ client/src/Pages/Account/UserSettings.jsx
@@ -2,11 +2,13 @@ import React from "react"
 import "./styles/usersettings.css"
 import ChangePassword from "../../Components/ChangePassword"
 import DeleteUser from "../../Components/DeleteUser"
+import ChangeEmail from "../../Components/ChangeEmail"
 
 function UserSettings() {
     return (
         <section id="user-settings-container">
             <ChangePassword />
+            <ChangeEmail />
             <DeleteUser />
         </section>
     )
