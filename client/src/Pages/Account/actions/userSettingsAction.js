3bc246ade2dede55d9624cd6587438e12c9ca684
jukry <94563540+jukry@users.noreply.github.com>
2023-10-22 20:29:38 +0300

    refactor: action now handles password and email change

--- client/src/Pages/Account/actions/userSettingsAction.js
+++ client/src/Pages/Account/actions/userSettingsAction.js
@@ -1,13 +1,23 @@
 import { redirect } from "react-router-dom"
-import { changePassword } from "../../../utils/utils"
+import { changeEmail, changePassword } from "../../../utils/utils"
+import { useLogout } from "../../../Hooks/useLogout"
 
 export async function action({ request }) {
     const formData = Object.fromEntries(await request.formData())
 
-    const res = await changePassword(formData)
-    if (!res.ok) {
-        return res.status
-    } else {
-        return redirect("/account")
+    if (formData.oldPassword) {
+        const res = await changePassword(formData)
+        if (!res.ok) {
+            return res.status
+        } else {
+            return redirect("/account")
+        }
+    } else if (formData.changeemail) {
+        const res = await changeEmail(formData)
+        if (!res.ok) {
+            return res.status
+        } else {
+            return res
+        }
     }
 }
