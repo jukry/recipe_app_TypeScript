dfe1825910bafa15b2dfb45c0bdbb96723d83f64
jukry <94563540+jukry@users.noreply.github.com>
2023-10-25 10:50:19 +0300

    feat: added multer and route for handling image

--- server/routes/recipes.js
+++ server/routes/recipes.js
@@ -7,10 +7,13 @@ import {
     createRecipe,
     deleteRecipe,
     updateRecipe,
+    uploadRecipeImage,
 } from "../controllers/recipeController.js"
 import protect from "../middleware/loginMiddleware.js"
 import newRecipeLimiter from "../middleware/newRecipeLimiter.js"
-
+import multer from "multer"
+const storage = multer.memoryStorage()
+const upload = multer({ storage })
 // route for get all recipes and create recipe
 recipesRouter
     .route("/")
@@ -26,5 +29,24 @@ recipesRouter
     .get(getRecipeById)
     .delete(protect, deleteRecipe)
     .patch(protect, updateRecipe)
+
+recipesRouter
+    .route("/upload")
+    .post(upload.single("imageUpload"), async function (req, res) {
+        if (!req.file) {
+            return res.json({ Message: "No image" })
+        }
+        try {
+            const b64 = Buffer.from(req.file.buffer).toString("base64")
+            let dataURI = "data:" + req.file.mimetype + ";base64," + b64
+            const cldRes = await uploadRecipeImage(dataURI)
+            res.json(cldRes)
+        } catch (error) {
+            console.log(error)
+            res.send({
+                message: error.message,
+            })
+        }
+    })
 
 export default recipesRouter
