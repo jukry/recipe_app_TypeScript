7cd14dfc321f229e6070c09a25b5d3e2a91d180a
jukry <94563540+jukry@users.noreply.github.com>
2023-10-27 15:25:32 +0300

    feat: routes for delete and get comments

--- server/routes/comments.js
+++ server/routes/comments.js
@@ -2,12 +2,18 @@ import express from "express"
 const commentsRouter = express.Router()
 
 import protect from "../middleware/loginMiddleware.js"
-import { getComments, postComment } from "../controllers/commentController.js"
+import {
+    deleteComment,
+    getAllComments,
+    getComments,
+    postComment,
+} from "../controllers/commentController.js"
 import newCommentLimiter from "../middleware/newCommentLimiter.js"
 
 // route for comment requests
 commentsRouter.route("/").post(protect, newCommentLimiter, postComment)
+commentsRouter.route("/").get(getAllComments)
 
-commentsRouter.route("/:id").get(getComments)
+commentsRouter.route("/:id").get(getComments).delete(deleteComment)
 
 export default commentsRouter
