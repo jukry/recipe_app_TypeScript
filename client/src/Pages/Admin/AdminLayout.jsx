4aef2b8090c05784c61df76ac9ecc5a7be91ed6b
jukry <94563540+jukry@users.noreply.github.com>
2023-10-27 15:16:24 +0300

    feat: layout for admin panel

---
+++ client/src/Pages/Admin/AdminLayout.jsx
@@ -0,0 +1,47 @@
+import React, { useContext } from "react"
+import { UserContext } from "../../Context/UserContext"
+import { NavLink, Navigate, Outlet } from "react-router-dom"
+import "./styles/adminLayout.css"
+export default function AdminLayout() {
+    const { adminMode } = useContext(UserContext)
+
+    if (!adminMode) {
+        return <Navigate to="/account" />
+    }
+    const activeStyle = {
+        fontWeight: "bold",
+        textDecoration: "underline",
+    }
+    return (
+        <section id="admin-layout-container">
+            <nav id="admin-nav">
+                <NavLink
+                    style={({ isActive }) => (isActive ? activeStyle : null)}
+                    to="."
+                    end
+                >
+                    Alkunäkymä
+                </NavLink>
+                <NavLink
+                    to="recipes"
+                    style={({ isActive }) => (isActive ? activeStyle : null)}
+                >
+                    Näytä kaikki reseptit
+                </NavLink>
+                <NavLink
+                    to="users"
+                    style={({ isActive }) => (isActive ? activeStyle : null)}
+                >
+                    Näytä kaikki käyttäjät
+                </NavLink>
+                <NavLink
+                    to="comments"
+                    style={({ isActive }) => (isActive ? activeStyle : null)}
+                >
+                    Näytä kaikki kommentit
+                </NavLink>
+            </nav>
+            <Outlet />
+        </section>
+    )
+}
