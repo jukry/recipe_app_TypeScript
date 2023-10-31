c9bb73f0cbd67c54fb9ba3797505cc17a9e9ccb5
jukry <94563540+jukry@users.noreply.github.com>
2023-10-31 09:49:07 +0200

    refactor: new rows handled as controlled inputs

--- client/src/Components/IngredientInputWrapper.jsx
+++ client/src/Components/IngredientInputWrapper.jsx
@@ -1,16 +1,46 @@
-import React from "react"
+import React, { useState } from "react"
 import IngredientInput from "./IngredientInput"
 import "../Pages/Account/styles/newRecipe.css"
-import { addRow } from "../utils/utils"
 
 export default function IngredientInputWrapper({ props }) {
+    const [extraInputs, setExtraInputs] = useState([])
+    const [extraInputNumber, setExtraInputNumber] = useState(5)
     return (
         <fieldset id="ingredients">
             <legend>Anna reseptin ainesosat</legend>
-            <button type="button" id="button-add-ingredient" onClick={addRow}>
+            <button
+                type="button"
+                id="button-add-ingredient"
+                onClick={() => {
+                    setExtraInputNumber((prev) => prev + 1)
+                    setExtraInputs(
+                        extraInputs.concat(
+                            <section
+                                className="ingredient-wrapper"
+                                key={`ingInput${extraInputNumber}`}
+                            >
+                                <IngredientInput
+                                    type="text"
+                                    placeholder="Anna ainesosan määrä (esim. 200g)"
+                                    className="new-recipe-ingredient-amount"
+                                    name={`amount${extraInputNumber}`}
+                                    onChange={props.handleChange}
+                                />
+                                <IngredientInput
+                                    type="text"
+                                    placeholder="Anna ainesosa"
+                                    className="new-recipe-ingredient"
+                                    name={`ingredient${5 + extraInputs.length}`}
+                                    onChange={props.handleChange}
+                                />
+                            </section>
+                        )
+                    )
+                }}
+            >
                 Lisää ainesosa
             </button>
-            <section className="ingredient-wrapper">
+            <section className="ingredient-wrapper" key={"ingInput1"}>
                 <IngredientInput
                     type="text"
                     placeholder="Anna ainesosan määrä (esim. 200g)"
@@ -30,7 +60,7 @@ export default function IngredientInputWrapper({ props }) {
                     required={true}
                 />
             </section>
-            <section className="ingredient-wrapper">
+            <section className="ingredient-wrapper" key={"ingInput2"}>
                 <IngredientInput
                     type="text"
                     placeholder="Anna ainesosan määrä (esim. 200g)"
@@ -48,7 +78,7 @@ export default function IngredientInputWrapper({ props }) {
                     value={props.recipe?.ingredient2 || ""}
                 />
             </section>
-            <section className="ingredient-wrapper">
+            <section className="ingredient-wrapper" key={"ingInput3"}>
                 <IngredientInput
                     type="text"
                     placeholder="Anna ainesosan määrä (esim. 200g)"
@@ -66,7 +96,7 @@ export default function IngredientInputWrapper({ props }) {
                     value={props.recipe?.ingredient3 || ""}
                 />
             </section>
-            <section className="ingredient-wrapper">
+            <section className="ingredient-wrapper" key={"ingInput4"}>
                 <IngredientInput
                     type="text"
                     placeholder="Anna ainesosan määrä (esim. 200g)"
@@ -84,6 +114,7 @@ export default function IngredientInputWrapper({ props }) {
                     value={props.recipe?.ingredient4 || ""}
                 />
             </section>
+            {extraInputs}
         </fieldset>
     )
 }
--- client/src/Components/RecipeStepsInput.jsx
+++ client/src/Components/RecipeStepsInput.jsx
@@ -1,12 +1,34 @@
-import React from "react"
-import { addRow } from "../utils/utils"
+import React, { useState } from "react"
 import "../Pages/Account/styles/newRecipe.css"
 
 export default function RecipeStepsInput({ props }) {
+    const [extraSteps, setExtraSteps] = useState([])
+    const [extraInputNumber, setExtraInputNumber] = useState(5)
+
     return (
         <fieldset id="recipe-steps">
             <legend>Anna reseptin valmistusohje</legend>
-            <button type="button" id="button-add-step" onClick={addRow}>
+            <button
+                type="button"
+                id="button-add-step"
+                onClick={() => {
+                    setExtraInputNumber((prev) => prev + 1)
+                    setExtraSteps(
+                        extraSteps.concat(
+                            <input
+                                type="text"
+                                placeholder="Kirjoita vaihe"
+                                className="new-recipe-step"
+                                id={`step-${extraInputNumber}`}
+                                name={`step${extraInputNumber}`}
+                                //value={props.recipe?.step1 || ""}
+                                onChange={(event) => props.handleChange(event)}
+                                key={`step${extraInputNumber}`}
+                            />
+                        )
+                    )
+                }}
+            >
                 Lisää vaihe
             </button>
             <input
@@ -18,6 +40,7 @@ export default function RecipeStepsInput({ props }) {
                 value={props.recipe?.step1 || ""}
                 onChange={(event) => props.handleChange(event)}
                 required={true}
+                key="step1"
             />
             <input
                 type="text"
@@ -27,6 +50,7 @@ export default function RecipeStepsInput({ props }) {
                 name="step2"
                 value={props.recipe?.step2 || ""}
                 onChange={(event) => props.handleChange(event)}
+                key="step2"
             />
             <input
                 type="text"
@@ -36,6 +60,7 @@ export default function RecipeStepsInput({ props }) {
                 name="step3"
                 value={props.recipe?.step3 || ""}
                 onChange={(event) => props.handleChange(event)}
+                key="step3"
             />
             <input
                 type="text"
@@ -45,7 +70,9 @@ export default function RecipeStepsInput({ props }) {
                 name="step4"
                 value={props.recipe?.step4 || ""}
                 onChange={(event) => props.handleChange(event)}
+                key="step4"
             />
+            {extraSteps}
         </fieldset>
     )
 }
