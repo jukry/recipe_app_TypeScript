f5d0c0e6971f2e91bbbd804cdece31bd60511d16
jukry <94563540+jukry@users.noreply.github.com>
2023-11-03 11:13:21 +0200

    refactor: controlled form and notification

--- client/src/Components/ChangeEmail.jsx
+++ client/src/Components/ChangeEmail.jsx
@@ -1,26 +1,53 @@
-import React, { useEffect } from "react"
-import { Form, useActionData, useNavigate } from "react-router-dom"
+import React, { useState } from "react"
 import { useUpdateUser } from "../Hooks/useUpdateUser"
+import { changeEmail } from "../utils/utils"
+import NotificationElement from "./NotificationElement"
 
 export default function ChangeEmail() {
     const { updateUser } = useUpdateUser()
-    const action = useActionData()
-    const navigate = useNavigate()
-    useEffect(() => {
-        if (action?.Message) {
-            updateUser(action.Message)
-            navigate("/account")
-        }
+    const [isLoading, setIsLoading] = useState(false)
+    const [emailData, setEmailData] = useState({
+        email: "",
+        password: "",
     })
+    const [showNotification, setShowNotification] = useState(false)
+    const [progress, setProgress] = useState(0)
+    const [notificationText, setNotificationText] = useState("")
+
+    async function handleSubmit(e) {
+        e.preventDefault()
+        setIsLoading(true)
+        const res = await changeEmail(emailData)
+        if (!res.ok) {
+            setNotificationText(
+                res.status === 403
+                    ? "Sähköpostisoite on jo olemassa"
+                    : res.status === 401
+                    ? "Salasana virheellinen"
+                    : "Jotain meni pieleen, yritä uudelleen"
+            )
+            setShowNotification(true)
+            setIsLoading(false)
+            return res.status
+        } else {
+            const body = await res.json()
+            updateUser(body.Message)
+            setNotificationText("Sähköpostiosoite vaihdettu")
+            setIsLoading(false)
+            setShowNotification(true)
+            return res
+        }
+    }
 
     return (
         <section id="change-email-container">
             <h3>Vaihda sähköpostiosoite</h3>
-            <Form
+            <form
                 name="change-email"
                 id="change-email-form"
                 method="post"
                 replace="true"
+                onSubmit={handleSubmit}
             >
                 <label htmlFor="change-email">
                     Kirjoita uusi sähköpostiosoite
@@ -28,22 +55,46 @@ export default function ChangeEmail() {
                 <input
                     type="email"
                     required
-                    name="changeemail"
+                    name="email"
                     id="change-email"
+                    value={emailData.email}
+                    onChange={(e) => {
+                        setEmailData((prev) => ({
+                            ...prev,
+                            [e.target.name]: e.target.value,
+                        }))
+                    }}
                 />
                 <label htmlFor="confirm-email-password">
                     Kirjoita nykyinen salasana
                 </label>
                 <input
                     type="password"
-                    name="confirmemailpassword"
+                    name="password"
                     id="confirm-email-password"
                     required
+                    value={emailData.password}
+                    onChange={(e) => {
+                        setEmailData((prev) => ({
+                            ...prev,
+                            [e.target.name]: e.target.value,
+                        }))
+                    }}
                 />
-                <button id="change-email-button">
-                    Vaihda sähköpostiosoite
+                <button disabled={isLoading} type="submit">
+                    {isLoading ? "Päivitetään..." : "Päivitä sähköpostiosoite"}
                 </button>
-            </Form>
+            </form>
+            {
+                <NotificationElement
+                    showNotification={showNotification}
+                    setProgress={setProgress}
+                    setShowNotification={setShowNotification}
+                    progress={progress}
+                    text={notificationText}
+                    onClickClose={true}
+                />
+            }
         </section>
     )
 }
