7752df50cf6787a010627520d96a24a3c472be3c
jukry <94563540+jukry@users.noreply.github.com>
2023-11-03 11:13:45 +0200

    refactor: controlled form and notification

--- client/src/Components/ChangePassword.jsx
+++ client/src/Components/ChangePassword.jsx
@@ -1,22 +1,63 @@
-import React from "react"
-import { Form, useNavigation } from "react-router-dom"
+import React, { useState } from "react"
+import NotificationElement from "./NotificationElement"
+import { changePassword } from "../utils/utils"
 
 export default function ChangePassword() {
-    const navigation = useNavigation()
+    const [showNotification, setShowNotification] = useState(false)
+    const [isLoading, setIsLoading] = useState(false)
+    const [progress, setProgress] = useState(0)
+    const [passwordData, setPasswordData] = useState({
+        oldPassword: "",
+        newPassword: "",
+        newRePassword: "",
+    })
+    const [notificationText, setNotificationText] = useState("")
+
+    async function handleSubmit(e) {
+        e.preventDefault()
+        setIsLoading(true)
+        const res = await changePassword(passwordData)
+        if (!res.ok) {
+            setNotificationText(
+                res.status === 401
+                    ? "Annoit väärän salasanan"
+                    : res.status === 400
+                    ? "Uusi salasana virheellinen"
+                    : "Jotain meni pieleen, yritä uudelleen"
+            )
+            setShowNotification(true)
+            setIsLoading(false)
+            return res.status
+        } else {
+            setNotificationText("Salasana vaihdettu")
+            setShowNotification(true)
+            setIsLoading(false)
+            return res.status
+        }
+    }
+
     return (
         <section id="change-password-container">
             <h3>Vaihda salasana</h3>
-            <Form
+            <form
                 name="change-password"
                 replace="true"
                 id="password-form"
                 method="post"
+                onSubmit={handleSubmit}
             >
                 <label htmlFor="oldpassword">Kirjoita nykyinen salasana</label>
                 <input
                     type="password"
                     name="oldPassword"
                     id="oldpassword"
+                    value={passwordData.oldPassword}
+                    onChange={(e) => {
+                        setPasswordData((prev) => ({
+                            ...prev,
+                            [e.target.name]: e.target.value,
+                        }))
+                    }}
                     required
                 />
                 <label htmlFor="newpassword">Kirjoita uusi salasana</label>
@@ -24,6 +65,13 @@ export default function ChangePassword() {
                     type="password"
                     name="newPassword"
                     id="newpassword"
+                    value={passwordData.newPassword}
+                    onChange={(e) => {
+                        setPasswordData((prev) => ({
+                            ...prev,
+                            [e.target.name]: e.target.value,
+                        }))
+                    }}
                     required
                 />
                 <label htmlFor="newrepassword">
@@ -33,17 +81,29 @@ export default function ChangePassword() {
                     type="password"
                     name="newRePassword"
                     id="newrepassword"
+                    value={passwordData.newRePassword}
+                    onChange={(e) => {
+                        setPasswordData((prev) => ({
+                            ...prev,
+                            [e.target.name]: e.target.value,
+                        }))
+                    }}
                     required
                 />
-                <button
-                    disabled={navigation.state === "submitting"}
-                    type="submit"
-                >
-                    {navigation.state === "idle"
-                        ? "Päivitä salasana"
-                        : "Päivitetään..."}
+                <button disabled={isLoading} type="submit">
+                    {isLoading ? "Päivitetään..." : "Päivitä salasana"}
                 </button>
-            </Form>
+            </form>
+            {
+                <NotificationElement
+                    showNotification={showNotification}
+                    setProgress={setProgress}
+                    setShowNotification={setShowNotification}
+                    progress={progress}
+                    text={notificationText}
+                    onClickClose={true}
+                />
+            }
         </section>
     )
 }
