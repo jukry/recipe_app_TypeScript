a7665eb3f3af889304601836ba1ac5a51692bb88
jukry <94563540+jukry@users.noreply.github.com>
2023-11-03 10:58:19 +0200

    feat: notification element with styling

---
+++ client/src/Components/NotificationElement.jsx
@@ -0,0 +1,46 @@
+import { useEffect } from "react"
+import "./Styles/notificationElement.css"
+
+export default function NotificationElement({
+    showNotification,
+    setShowNotification,
+    progress,
+    text,
+    onClickClose = true,
+    setProgress,
+}) {
+    useEffect(() => {
+        if (showNotification && progress < 100) {
+            setTimeout(() => {
+                setProgress((prev) => prev + 1)
+            }, 50)
+        }
+        if (progress === 100) {
+            setShowNotification(false)
+            setTimeout(() => {
+                setProgress(0)
+            }, 500)
+        }
+        return () => {
+            clearTimeout()
+        }
+    }, [progress, showNotification])
+    return (
+        <section
+            className={`notification-modal ${
+                showNotification ? "show-notification" : "hide-notification"
+            }`}
+            onClick={() => {
+                if (onClickClose) {
+                    setShowNotification(!onClickClose)
+                    setTimeout(() => {
+                        setProgress(0)
+                    }, 500)
+                }
+            }}
+        >
+            <p>{text}</p>
+            <div className="progress" style={{ width: `${progress}%` }}></div>
+        </section>
+    )
+}
