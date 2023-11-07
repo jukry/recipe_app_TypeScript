7e1e9481bac660f96a77465f8b48e07cb42f5fd3
jukry <94563540+jukry@users.noreply.github.com>
2023-11-07 08:33:56 +0200

    refactor: using ref and state to handle deleting and adding new inputs

--- client/src/Components/IngredientInputWrapper.jsx
+++ client/src/Components/IngredientInputWrapper.jsx
@@ -1,10 +1,13 @@
-import React, { useState } from "react"
+import React, { useRef, useState } from "react"
 import IngredientInput from "./IngredientInput"
 import "../Pages/Account/styles/newRecipe.css"
 
 export default function IngredientInputWrapper({ props }) {
     const [extraInputs, setExtraInputs] = useState([])
     const [extraInputNumber, setExtraInputNumber] = useState(5)
+    const [keyValue, setKeyValue] = useState(5)
+    const ingredientsRef = useRef([])
+
     return (
         <fieldset id="ingredients">
             <legend>Anna reseptin ainesosat</legend>
@@ -13,11 +16,19 @@ export default function IngredientInputWrapper({ props }) {
                 id="button-add-ingredient"
                 onClick={() => {
                     setExtraInputNumber((prev) => prev + 1)
+                    props.setRecipe((prev) => ({
+                        ...prev,
+                        [`amount${extraInputNumber}`]: "",
+                        [`ingredient${extraInputNumber}`]: "",
+                    }))
                     setExtraInputs(
                         extraInputs.concat(
                             <section
                                 className="ingredient-wrapper"
-                                key={`ingInput${extraInputNumber}`}
+                                key={`ingInput${keyValue}`}
+                                ref={(el) =>
+                                    (ingredientsRef[extraInputNumber - 1] = el)
+                                }
                             >
                                 <IngredientInput
                                     type="text"
@@ -30,17 +41,36 @@ export default function IngredientInputWrapper({ props }) {
                                     type="text"
                                     placeholder="Anna ainesosa"
                                     className="new-recipe-ingredient"
-                                    name={`ingredient${5 + extraInputs.length}`}
+                                    name={`ingredient${extraInputNumber}`}
                                     onChange={props.handleChange}
                                 />
+                                <button
+                                    className="delete-ingredient-button"
+                                    type="button"
+                                    id={`ingButton${extraInputNumber}`}
+                                    onClick={(e) => {
+                                        const indexToDelete =
+                                            e.target.id.split("ingButton")[1] -
+                                            1
+                                        props.handleIngredientDelete(e)
+                                        ingredientsRef[indexToDelete].remove()
+                                    }}
+                                >
+                                    -
+                                </button>
                             </section>
                         )
                     )
+                    setKeyValue((prev) => prev + 1)
                 }}
             >
                 Lisää ainesosa
             </button>
-            <section className="ingredient-wrapper" key={"ingInput1"}>
+            <section
+                className="ingredient-wrapper"
+                key={"ingInput1"}
+                ref={(el) => (ingredientsRef[0] = el)}
+            >
                 <IngredientInput
                     type="text"
                     placeholder="Anna ainesosan määrä (esim. 200g)"
@@ -60,7 +90,11 @@ export default function IngredientInputWrapper({ props }) {
                     required={true}
                 />
             </section>
-            <section className="ingredient-wrapper" key={"ingInput2"}>
+            <section
+                className="ingredient-wrapper"
+                key={"ingInput2"}
+                ref={(el) => (ingredientsRef[1] = el)}
+            >
                 <IngredientInput
                     type="text"
                     placeholder="Anna ainesosan määrä (esim. 200g)"
@@ -77,8 +111,23 @@ export default function IngredientInputWrapper({ props }) {
                     onChange={props.handleChange}
                     value={props.recipe?.ingredient2 || ""}
                 />
+                <button
+                    className="delete-ingredient-button"
+                    type="button"
+                    id="ingButton2"
+                    onClick={(e) => {
+                        props.handleIngredientDelete(e)
+                        ingredientsRef[1].remove()
+                    }}
+                >
+                    -
+                </button>
             </section>
-            <section className="ingredient-wrapper" key={"ingInput3"}>
+            <section
+                className="ingredient-wrapper"
+                key={"ingInput3"}
+                ref={(el) => (ingredientsRef[2] = el)}
+            >
                 <IngredientInput
                     type="text"
                     placeholder="Anna ainesosan määrä (esim. 200g)"
@@ -95,8 +144,23 @@ export default function IngredientInputWrapper({ props }) {
                     onChange={props.handleChange}
                     value={props.recipe?.ingredient3 || ""}
                 />
+                <button
+                    className="delete-ingredient-button"
+                    type="button"
+                    id="ingButton3"
+                    onClick={(e) => {
+                        props.handleIngredientDelete(e)
+                        ingredientsRef[2].remove()
+                    }}
+                >
+                    -
+                </button>
             </section>
-            <section className="ingredient-wrapper" key={"ingInput4"}>
+            <section
+                className="ingredient-wrapper"
+                key={"ingInput4"}
+                ref={(el) => (ingredientsRef[3] = el)}
+            >
                 <IngredientInput
                     type="text"
                     placeholder="Anna ainesosan määrä (esim. 200g)"
@@ -113,6 +177,17 @@ export default function IngredientInputWrapper({ props }) {
                     onChange={props.handleChange}
                     value={props.recipe?.ingredient4 || ""}
                 />
+                <button
+                    className="delete-ingredient-button"
+                    type="button"
+                    id="ingButton4"
+                    onClick={(e) => {
+                        props.handleIngredientDelete(e)
+                        ingredientsRef[3].remove()
+                    }}
+                >
+                    -
+                </button>
             </section>
             {extraInputs}
         </fieldset>
