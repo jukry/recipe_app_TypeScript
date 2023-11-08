dcbe399cb03306a4646252ca5518452a473cfb1f
jukry <94563540+jukry@users.noreply.github.com>
2023-11-08 14:18:41 +0200

    feat: tabindex, ARIAs

--- client/src/Components/ShowMobileNavButton.jsx
+++ client/src/Components/ShowMobileNavButton.jsx
@@ -3,7 +3,12 @@ import "./Styles/mobileNavButton.css"
 
 export default function ShowMobileNavButton({ showNav, handleNavClick }) {
     return (
-        <div onClick={(e) => handleNavClick(e)} className="nav-button">
+        <div
+            onClick={(e) => handleNavClick(e)}
+            className="nav-button"
+            aria-label="Valikko"
+            tabIndex={showNav ? 2 : 0}
+        >
             <span
                 className={`${!showNav ? "line line-1" : "cross cross-1"}`}
             ></span>
