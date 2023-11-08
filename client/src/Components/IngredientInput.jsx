a4a46ac5f8d882587f88c6a41231387929b3dec7
jukry <94563540+jukry@users.noreply.github.com>
2023-11-08 14:12:30 +0200

    refactor: label, id prop

--- client/src/Components/IngredientInput.jsx
+++ client/src/Components/IngredientInput.jsx
@@ -2,15 +2,21 @@ import React from "react"
 
 export default function IngredientInput(props) {
     return (
-        <input
-            type={props.type}
-            placeholder={props.placeholder}
-            className={props.className}
-            name={props.name}
-            required={props.required}
-            value={props.value}
-            onChange={(event) => props.onChange(event)}
-            autoComplete="off"
-        />
+        <>
+            <label htmlFor={props.id} className="visuallyhidden">
+                {props.placeholder}
+            </label>
+            <input
+                type={props.type}
+                placeholder={props.placeholder}
+                className={props.className}
+                name={props.name}
+                id={props.id}
+                required={props.required}
+                value={props.value}
+                onChange={(event) => props.onChange(event)}
+                autoComplete="off"
+            />
+        </>
     )
 }
