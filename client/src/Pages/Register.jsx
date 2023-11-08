6fc618422fa81cdca9b4b453ac9ad4e69bed4019
jukry <94563540+jukry@users.noreply.github.com>
2023-11-08 14:21:48 +0200

    feat: ref to focus on email, tabindex, accessible error text

--- client/src/Pages/Register.jsx
+++ client/src/Pages/Register.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react"
+import React, { useContext, useEffect, useRef } from "react"
 import { Form, Navigate, useActionData, useNavigation } from "react-router-dom"
 import "./styles/Register.css"
 import { UserContext } from "../Context/UserContext"
@@ -7,14 +7,20 @@ export default function Register() {
     const navigation = useNavigation()
     const action = useActionData()
     const { user } = useContext(UserContext)
+    const inputRef = useRef(null)
+
+    useEffect(() => {
+        //for accessibility
+        inputRef.current.focus()
+    }, [])
     if (user.id) {
         return <Navigate to="/account" />
     }
     return (
         <section id="register-container">
-            <h2>Rekisteröidy</h2>
+            <h2 tabIndex={0}>Rekisteröidy</h2>
             {action && (
-                <h3 id="check-register-input">
+                <h3 id="check-register-input" tabIndex={0}>
                     {action === 406
                         ? "Salasanat eivät täsmää"
                         : action === 401
@@ -27,24 +33,61 @@ export default function Register() {
                 </h3>
             )}
             <Form replace="true" method="post" id="register-form">
+                <label htmlFor="email" className="visuallyhidden">
+                    {action === 406
+                        ? "Salasanat eivät täsmää"
+                        : action === 401
+                        ? "Täytä kaikki kentät"
+                        : action === 403
+                        ? "Sähköposti on jo käytössä"
+                        : action === 400
+                        ? "Jotain meni pieleen, yritä uudelleen"
+                        : ""}
+                </label>
                 <input
                     type="email"
                     name="email"
+                    id="email"
                     placeholder="Sähköpostiosoite"
                     required
                     autoComplete="email"
+                    ref={inputRef}
                 />
+                <label htmlFor="password" className="visuallyhidden">
+                    {action === 406
+                        ? "Salasanat eivät täsmää"
+                        : action === 401
+                        ? "Täytä kaikki kentät"
+                        : action === 403
+                        ? "Sähköposti on jo käytössä"
+                        : action === 400
+                        ? "Jotain meni pieleen, yritä uudelleen"
+                        : ""}
+                </label>
                 <input
                     type="password"
                     name="password"
                     placeholder="Salasana"
+                    id="password"
                     required
                     pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
                     title="Salasanassa pitää olla ainakin yksi numero, yksi pieni kirjain, yksi iso kirjain sekä vähintään 8 merkkiä"
                 />
+                <label htmlFor="re-password" className="visuallyhidden">
+                    {action === 406
+                        ? "Salasanat eivät täsmää"
+                        : action === 401
+                        ? "Täytä kaikki kentät"
+                        : action === 403
+                        ? "Sähköposti on jo käytössä"
+                        : action === 400
+                        ? "Jotain meni pieleen, yritä uudelleen"
+                        : ""}
+                </label>
                 <input
                     type="password"
                     name="re-password"
+                    id="re-password"
                     placeholder="Kirjoita salasana uudelleen"
                     required
                     pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
