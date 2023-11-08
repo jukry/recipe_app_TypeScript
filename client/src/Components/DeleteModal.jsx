afcc4ebd3e0caedf92b00e0072ed0095445c6048
jukry <94563540+jukry@users.noreply.github.com>
2023-11-08 14:10:59 +0200

    feat: ref focus, tabindexes, ARIAs

--- client/src/Components/DeleteModal.jsx
+++ client/src/Components/DeleteModal.jsx
@@ -1,12 +1,18 @@
-import React from "react"
+import React, { useEffect, useRef } from "react"
 import "./Styles/deleteModal.css"
 
 export default function DeleteModal(props) {
+    const deleteRef = useRef(null)
+
+    useEffect(() => {
+        //for accessibility
+        deleteRef.current.focus()
+    }, [])
     return (
         <div id="backdrop-blur" onClick={props.onClose}>
             <div className="delete-modal">
                 <section className="modal-wrapper">
-                    <h4>
+                    <h4 tabIndex={0} ref={deleteRef}>
                         {props.props.text} {props.props.name}
                     </h4>
                     <div id="delete-buttons-container">
@@ -15,7 +21,11 @@ export default function DeleteModal(props) {
                         </button>
                         <button id="delete-no">En</button>
                     </div>
-                    <button id="modal-close-button" onClick={props.onClose}>
+                    <button
+                        id="modal-close-button"
+                        onClick={props.onClose}
+                        aria-label="Sulje"
+                    >
                         X
                     </button>
                 </section>
