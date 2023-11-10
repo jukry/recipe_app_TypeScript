66b06cba17925ac0c10f90a00af0805f65742942
jukry <94563540+jukry@users.noreply.github.com>
2023-11-10 10:47:46 +0200

    feat: render tagsinput component

--- client/src/Components/RecipeDataContainer.jsx
+++ client/src/Components/RecipeDataContainer.jsx
@@ -4,11 +4,13 @@ import RecipeStepsInput from "./RecipeStepsInput"
 import RecipeDataInput from "./RecipeDataInput"
 import "../Pages/Account/styles/newRecipe.css"
 import RecipeImageInput from "./RecipeImageInput"
+import RecipeTagsInput from "./RecipeTagsInput"
 
 export default function RecipeDataContainer({ props }) {
     return (
         <div id="recipe-data-container">
             <RecipeDataInput props={props} />
+            <RecipeTagsInput props={[props.recipe, props.setRecipe]} />
             <div id="recipe-ingredient-steps-wrapper">
                 <IngredientInputWrapper props={props} />
                 <RecipeStepsInput props={props} />
