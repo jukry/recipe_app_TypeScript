45f989b15b111ef1a542091d06b289f5e067ca62
jukry <94563540+jukry@users.noreply.github.com>
2023-12-05 14:34:17 +0200

    refactor: typed component

--- client/src/Pages/Account/AddNewRecipe.tsx
+++ client/src/Pages/Account/AddNewRecipe.tsx
@@ -1,55 +1,66 @@
 import "./styles/newRecipe.css"
 import RecipeDataContainer from "../../Components/RecipeDataContainer"
-import { useState } from "react"
+import { ChangeEvent, FormEvent, useState } from "react"
 
 function AddNewRecipe() {
     document.title = "Lisää uusi resepti"
-    const [recipe, setRecipe] = useState([])
-    const [file, setFile] = useState({})
-    const [previewFile, setPreviewFile] = useState()
+    const [recipe, setRecipe] = useState({})
+    const [file, setFile] = useState<Blob | undefined>()
+    const [previewFile, setPreviewFile] = useState<string | undefined>()
     const [isLoading, setIsLoading] = useState(false)
-
-    function handleChange(e) {
+    function handleChange(e: ChangeEvent<HTMLInputElement>) {
         setRecipe((prev) => ({ ...prev, [e.target.name]: e.target.value }))
     }
-
-    function handleIngredientDelete(e) {
-        const index = e.target.id.split("ingButton")[1]
-        setRecipe((prev) => {
-            delete prev[`amount${index}`]
-            delete prev[`ingredient${index}`]
+    console.log(recipe)
+    function handleIngredientDelete(e: ChangeEvent<HTMLInputElement>) {
+        const index = e?.target?.id.split("ingButton")[1]
+        setRecipe((prev: {}) => {
+            const amountProperty: string = `amount${index}`
+            const ingredientProperty: string = `ingredient${index}`
+            delete prev[amountProperty as keyof {}]
+            delete prev[ingredientProperty as keyof {}]
             return prev
         })
     }
 
-    function handleStepDelete(e) {
+    function handleStepDelete(e: ChangeEvent<HTMLInputElement>) {
         const index = e.target.id.split("stepButton")[1]
-        setRecipe((prev) => {
+        setRecipe((prev: {}) => {
             const step = `step${index}`
-            delete prev[step]
+            delete prev[step as keyof {}]
             return prev
         })
     }
 
-    function handleFileChange(e) {
+    function handleFileChange(e: ChangeEvent<HTMLInputElement>) {
         e.preventDefault()
+        if (!e.target.files) {
+            return
+        }
+        const imageFile: Blob = e.target.files[0]
         if (
             e.target.files?.length === 0 ||
             e.target.id === "delete-image-button"
         ) {
-            setFile({})
+            setFile(undefined)
             setPreviewFile(undefined)
             return
         }
-        setFile(e.target.files[0])
-        setPreviewFile(URL.createObjectURL(e.target.files[0]))
+        const URLImageObject = URL.createObjectURL(imageFile)
+        setFile(imageFile)
+        setPreviewFile(URLImageObject)
+        console.log(URLImageObject)
     }
 
-    async function handleSubmit(e) {
+    async function handleSubmit(e: FormEvent) {
         setIsLoading(true)
         e.preventDefault()
         const formData = new FormData()
-        formData.append("imageUpload", file)
+        console.log(file)
+        if (file) {
+            console.log("IF FILE")
+            formData.append("imageUpload", file)
+        }
 
         const imageSend = await fetch(
             process.env.NODE_ENV === "production"
@@ -83,7 +94,7 @@ function AddNewRecipe() {
         if (!recipeRes.ok) {
             return recipeRes.status
         } else {
-            location.replace("/account/myrecipes")
+            //location.replace("/account/myrecipes")
         }
     }
     return (
@@ -96,15 +107,23 @@ function AddNewRecipe() {
             >
                 <RecipeDataContainer
                     props={{
-                        recipe,
-                        handleChange,
-                        handleFileChange,
-                        previewFile,
-                        file,
-                        setRecipe,
-                        handleIngredientDelete,
-                        handleStepDelete,
+                        recipe: recipe,
+                        handleChange: handleChange,
+                        handleFileChange: handleFileChange,
+                        previewFile: previewFile,
+                        file: file,
+                        setRecipe: setRecipe,
+                        handleIngredientDelete: handleIngredientDelete,
+                        handleStepDelete: handleStepDelete,
                     }}
+                    /* recipe={recipe}
+                    handleChange={handleChange}
+                    handleFileChange={handleFileChange}
+                    previewFile={previewFile}
+                    file={file}
+                    setRecipe={setRecipe}
+                    handleIngredientDelete={handleIngredientDelete}
+                    handleStepDelete={handleStepDelete} */
                 />
                 <button disabled={isLoading}>
                     {!isLoading ? "Lähetä resepti" : "Lähetetään..."}
