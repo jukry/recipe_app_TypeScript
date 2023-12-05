b2e8c94cd6b6b934d561f38cdc67123f9355bfd3
jukry <94563540+jukry@users.noreply.github.com>
2023-12-05 14:34:33 +0200

    refactor: typed component

--- client/src/Components/RecipeDataContainer.tsx
+++ client/src/Components/RecipeDataContainer.tsx
@@ -1,12 +1,14 @@
-import React from "react"
+import React, { ChangeEvent } from "react"
 import IngredientInputWrapper from "./IngredientInputWrapper"
 import RecipeStepsInput from "./RecipeStepsInput"
 import RecipeDataInput from "./RecipeDataInput"
 import "../Pages/Account/styles/newRecipe.css"
 import RecipeImageInput from "./RecipeImageInput"
 import RecipeTagsInput from "./RecipeTagsInput"
+import { RecipeProps } from "../utils/APIResponseTypes"
 
-export default function RecipeDataContainer({ props }) {
+export default function RecipeDataContainer({ props }: RecipeProps) {
+    console.log(props)
     return (
         <div id="recipe-data-container">
             <RecipeDataInput props={props} />
