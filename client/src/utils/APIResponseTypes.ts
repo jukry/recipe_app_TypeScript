1c1929cf1edbb1600cd15a7cfa6660ad7c2108f0
jukry <94563540+jukry@users.noreply.github.com>
2023-12-06 13:01:21 +0200

    refactor: changed file Blob to File interface; feat: handleFileDelete

--- client/src/utils/APIResponseTypes.ts
+++ client/src/utils/APIResponseTypes.ts
@@ -1,4 +1,4 @@
-import { ChangeEvent } from "react"
+import { ChangeEvent, MouseEvent } from "react"
 
 export interface IUser {
     id: string
@@ -13,9 +13,10 @@ export interface IUser {
 export type RecipeProps = {
     props: {
         recipe: {}
-        file: Blob | undefined
+        file: File | undefined
         handleChange: (e: ChangeEvent<HTMLInputElement>) => void
         handleFileChange: (e: ChangeEvent<HTMLInputElement>) => void
+        handleFileDelete: (e: MouseEvent<HTMLButtonElement>) => void
         handleIngredientDelete: (e: ChangeEvent<HTMLInputElement>) => void
         handleStepDelete: (e: ChangeEvent<HTMLInputElement>) => void
         previewFile: string | undefined
