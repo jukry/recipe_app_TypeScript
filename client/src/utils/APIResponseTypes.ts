bb622946484ee89275895d47b16abbce1656d2b9
jukry <94563540+jukry@users.noreply.github.com>
2024-02-25 13:17:32 +0200

    feat: new interfaces

--- client/src/utils/APIResponseTypes.ts
+++ client/src/utils/APIResponseTypes.ts
@@ -10,15 +10,54 @@ export interface IUser {
     comments: string[]
 }
 
+export interface IRecipeDetails {
+    _id: string
+    name: string
+    description: string
+    images: string[]
+    tags: string[]
+    ingredients: {}[]
+    instructions: string[]
+    comments: string[]
+}
+export interface IRecipe {
+    _id: string
+    name: string
+    description: string
+    images: string[]
+    tags: string[]
+    ingredients: {}[]
+    instructions: string[]
+    comments: string[]
+    user: {
+        email: string
+        id: string
+    }
+}
+
+export interface INewRecipe {
+    name?: string
+    description?: string
+    images?: string[]
+    tags?: string[]
+    ingredients?: {}[]
+    instructions?: {
+        step1?: string
+        step2?: string
+        step3?: string
+        step4?: string
+    }
+}
+
 export type RecipeProps = {
     props: {
-        recipe: {}
+        recipe: INewRecipe
         file: File | undefined
         handleChange: (e: ChangeEvent<HTMLInputElement>) => void
         handleFileChange: (e: ChangeEvent<HTMLInputElement>) => void
         handleFileDelete: (e: MouseEvent<HTMLButtonElement>) => void
         handleIngredientDelete: (e: ChangeEvent<HTMLInputElement>) => void
-        handleStepDelete: (e: ChangeEvent<HTMLInputElement>) => void
+        handleStepDelete: (e: string) => void
         previewFile: string | undefined
         setRecipe: React.Dispatch<React.SetStateAction<{}>>
     }
