9417726586d189ccbdc1c79aaa21091532e21587
jukry <94563540+jukry@users.noreply.github.com>
2024-02-26 13:23:14 +0200

    refactor: typed Fooditem, using named props, fav-icon from p to button

--- client/src/Components/Fooditem.tsx
+++ client/src/Components/Fooditem.tsx
@@ -1,21 +1,34 @@
-import { useContext, useState } from "react"
+import { BaseSyntheticEvent, useContext, useState } from "react"
 import { Link } from "react-router-dom"
 import { UserContext } from "../Context/UserContext"
 import { createPortal } from "react-dom"
 import DeleteModal from "./DeleteModal"
 import { handleFavorite } from "../utils/utils"
-import { useMutation } from "@tanstack/react-query"
+import { UseMutationResult, useMutation } from "@tanstack/react-query"
 import { RecipesShownContext } from "../Context/RecipesShownContext"
+import {
+    IRecipe,
+    IRecipeDetails,
+    IUserContext,
+} from "../utils/APIResponseTypes"
 
-export default function Fooditem(props) {
-    const data = props.props[0]
-    const searchParams = props.props[1] ?? ""
-    const handleDelete = props.props[2]
+export default function Fooditem(props: {
+    item: IRecipeDetails | IRecipe
+    searchParams?: URLSearchParams
+    handleDelete?: UseMutationResult<
+        Response,
+        unknown,
+        { id: string | undefined }
+    >
+}) {
+    const data = props.item
+    const searchParams = props.searchParams ?? ""
+    const handleDelete = props.handleDelete
     const { setCurrentRecipe } = useContext(RecipesShownContext)
     const [showModal, setShowModal] = useState(false)
-    const { user, dispatch } = useContext(UserContext)
-    const { favrecipes } = user
-
+    const { user, dispatch } = useContext<IUserContext>(UserContext)
+    const favrecipes = user?.favrecipes
+    const container = document.getElementById("container")
     const { mutate, isLoading } = useMutation({
         //TODO optimistic update
         mutationFn: handleFavorite,
@@ -31,7 +44,8 @@ export default function Fooditem(props) {
                         referrer: location.pathname,
                     }}
                     onClick={() => {
-                        setCurrentRecipe(data.id)
+                        if (!setCurrentRecipe) return null
+                        setCurrentRecipe(data.id as string)
                     }}
                     className="food-item"
                     id={data.id || data._id}
@@ -54,7 +68,7 @@ export default function Fooditem(props) {
                                 {data.description}
                             </p>
                         </div>
-                        <p
+                        <button
                             className="fav-heart"
                             onClick={(event) => {
                                 event.stopPropagation()
@@ -68,7 +82,9 @@ export default function Fooditem(props) {
                             }}
                             disabled={isLoading}
                         >
-                            {favrecipes?.includes(data.id || data._id) ? (
+                            {favrecipes?.includes(
+                                (data.id as string) || (data._id as string)
+                            ) ? (
                                 <span
                                     title="Poista resepti suosikeista"
                                     className="isfav"
@@ -89,7 +105,7 @@ export default function Fooditem(props) {
                                     &#x2661;
                                 </span>
                             )}
-                        </p>
+                        </button>
                     </div>
                 </Link>
                 <div className="recipe-management">
@@ -120,18 +136,22 @@ export default function Fooditem(props) {
                         ""
                     )}
                     {showModal &&
+                        container &&
                         createPortal(
                             <DeleteModal
                                 data={{
                                     text: "Haluatko poistaa reseptin ",
                                     name: data.name + "?",
                                 }}
-                                onDelete={(event) => {
+                                onDelete={(event: BaseSyntheticEvent) => {
                                     if (event.target.id === "delete-yes") {
-                                        handleDelete(data._id)
+                                        if (!handleDelete) return null
+                                        handleDelete.mutateAsync({
+                                            id: data._id,
+                                        })
                                     }
                                 }}
-                                onClose={(event) => {
+                                onClose={(event: BaseSyntheticEvent) => {
                                     if (
                                         event.target.className !==
                                         "delete-modal"
@@ -141,7 +161,7 @@ export default function Fooditem(props) {
                                     }
                                 }}
                             />,
-                            document.getElementById("container")
+                            container
                         )}
                 </div>
             </>
