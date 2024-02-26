b92b997b6f22835ffbcc6009b2ea9bed3329acbc
jukry <94563540+jukry@users.noreply.github.com>
2024-02-26 08:24:13 +0200

    refactor: user context import to tsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,7 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App"
 import "./index.css"
-import { UserContextProvider } from "./Context/UserContext"
+import { UserContextProvider } from "./Context/UserContext.tsx"
 import { RecipesShownContextProvider } from "./Context/RecipesShownContext"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
