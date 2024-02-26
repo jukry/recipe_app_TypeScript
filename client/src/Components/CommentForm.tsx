488064557ccc17c5a78bf74309051319f7dc0285
jukry <94563540+jukry@users.noreply.github.com>
2024-02-26 16:17:44 +0200

    refactor: typed component

--- client/src/Components/CommentForm.tsx
+++ client/src/Components/CommentForm.tsx
@@ -1,36 +1,32 @@
-import React, { useContext, useState } from "react"
+import React, { ChangeEvent, useContext, useState } from "react"
 import "./Styles/recipeComments.css"
 import { useParams } from "react-router-dom"
 import { UserContext } from "../Context/UserContext"
-import { useMutation } from "@tanstack/react-query"
+import { UseMutationResult, useMutation } from "@tanstack/react-query"
+import { CommentMutation, IUserContext } from "../utils/APIResponseTypes"
 
-function CommentForm(props) {
+function CommentForm(props: { handleSubmit: CommentMutation }) {
     const { id } = useParams()
-    const { user } = useContext(UserContext)
+    const { user } = useContext<IUserContext>(UserContext)
     const [comment, setComment] = useState({
-        content: "",
-        username: "",
+        content: "asd",
+        username: "asd",
     })
-    const handleSubmit = props.props.mutate
-    const mutation = props.props
-    const handleCommentChange = (e) => {
+    const mutation = props.handleSubmit
+    const handleCommentChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
         setComment((prev) => ({
             ...prev,
             content: e.target.value,
         }))
     }
-    const handleCommentUsername = (e) => {
+    const handleCommentUsername = (e: ChangeEvent<HTMLInputElement>) => {
         setComment((prev) => ({
             ...prev,
             username: e.target.value,
         }))
     }
 
-    const { mutate, isLoading } = useMutation({
-        mutationFn: handleSubmit,
-    })
-
-    return !user.id ? (
+    return !user?.id ? (
         <p>Kirjaudu sisään jos haluat kommentoida reseptiä</p>
     ) : (
         <div id="comment-form-container">
@@ -46,9 +42,14 @@ function CommentForm(props) {
                 id="comment-form"
                 onSubmit={(event) => {
                     event.preventDefault()
-                    mutate([event, comment, id, user.id, setComment])
+                    mutation.mutate({
+                        event,
+                        comment,
+                        id: id as string,
+                        userId: user.id as string,
+                        setComment,
+                    })
                 }}
-                disabled={isLoading}
             >
                 <label
                     htmlFor="comment-box"
@@ -60,8 +61,8 @@ function CommentForm(props) {
                 <textarea
                     name="commentbox"
                     id="comment-box"
-                    cols="30"
-                    rows="5"
+                    cols={30}
+                    rows={5}
                     form="comment-form"
                     placeholder="Kirjoita kommenttisi"
                     maxLength={1000}
@@ -84,7 +85,11 @@ function CommentForm(props) {
                     maxLength={64}
                     spellCheck={false}
                 />
-                <button type="submit" id="comment-submit-button">
+                <button
+                    type="submit"
+                    id="comment-submit-button"
+                    disabled={mutation.isLoading}
+                >
                     Lisää kommentti
                 </button>
                 {mutation.isError ? (
