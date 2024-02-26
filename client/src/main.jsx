091f7c4759d3dc565448891f0ea65e30beeaa426
jukry <94563540+jukry@users.noreply.github.com>
2024-02-26 10:02:34 +0200

    refactor: recipesShowContext import to tsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client"
 import App from "./App"
 import "./index.css"
 import { UserContextProvider } from "./Context/UserContext.tsx"
-import { RecipesShownContextProvider } from "./Context/RecipesShownContext"
+import { RecipesShownContextProvider } from "./Context/RecipesShownContext.tsx"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <UserContextProvider>
