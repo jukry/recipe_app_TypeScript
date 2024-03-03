77dbd7385d3e48c77d3a4452e498c619eeb305b7
jukry <94563540+jukry@users.noreply.github.com>
2024-03-03 21:01:57 +0200

    refactor: typed component

--- client/src/Pages/Account/AccountDashboard.tsx
+++ client/src/Pages/Account/AccountDashboard.tsx
@@ -1,17 +1,18 @@
 import React, { useContext } from "react"
 import "./styles/account.css"
 import { UserContext } from "../../Context/UserContext"
+import { IUserContext } from "../../utils/APIResponseTypes"
 
 export default function AccountDashboard() {
-    const { user } = useContext(UserContext)
+    const { user } = useContext<IUserContext>(UserContext)
     document.title = "Omat tiedot"
 
     return (
         <section id="account-details">
             <h3 id="username" tabIndex={0}>
-                Tervetuloa {user.email}
+                Tervetuloa {user?.email}
             </h3>
-            {user.lastlogins.length === 2 ? (
+            {user?.lastlogins?.length === 2 ? (
                 <p id="lastlogin" tabIndex={0}>
                     Edellinen kirjautuminen:{" "}
                     {new Date(user.lastlogins[0]).toLocaleString("fi-FI")}
@@ -20,12 +21,14 @@ export default function AccountDashboard() {
                 ""
             )}
             <p id="recipesamount" tabIndex={0}>
-                Sinulla on {user.recipes.length}
-                {user.recipes.length === 1 ? " oma resepti" : " omaa reseptiä"}
+                Sinulla on {user?.recipes?.length}
+                {user?.recipes?.length === 1
+                    ? " oma resepti"
+                    : " omaa reseptiä"}
             </p>
             <p id="favrecipeamount" tabIndex={0}>
-                Sinulla on {user.favrecipes.length}
-                {user.favrecipes.length === 1
+                Sinulla on {user?.favrecipes?.length}
+                {user?.favrecipes?.length === 1
                     ? " suosikkiresepti"
                     : " suosikkireseptiä"}
             </p>
