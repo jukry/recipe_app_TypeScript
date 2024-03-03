2fa8bda8c6588ebe519a61eb6d99782e2610647c
jukry <94563540+jukry@users.noreply.github.com>
2024-03-03 21:34:52 +0200

    refactor: typed hook

--- client/src/Hooks/useLogout.ts
+++ client/src/Hooks/useLogout.ts
@@ -1,9 +1,12 @@
 import { useContext } from "react"
 import { UserContext } from "../Context/UserContext"
+import { IUserContext } from "../utils/APIResponseTypes"
+import { ILogoutHook } from "../Components/Navbar"
 
-export const useLogout = () => {
-    const { dispatch, setIsLoggedIn, setAdminMode } = useContext(UserContext)
-
+export const useLogout = (): ILogoutHook | null => {
+    const { dispatch, setIsLoggedIn, setAdminMode } =
+        useContext<IUserContext>(UserContext)
+    if (!setIsLoggedIn || !setAdminMode || !dispatch) return null
     const logout = async () => {
         const res = await fetch(
             process.env.NODE_ENV === "production"
@@ -19,6 +22,7 @@ export const useLogout = () => {
             setIsLoggedIn(false)
             setAdminMode(false)
         }
+        return res
     }
     return { logout }
 }
