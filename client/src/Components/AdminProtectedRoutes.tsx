a8c40314d4bb12262f73797abb3308ec0fa5cb75
jukry <94563540+jukry@users.noreply.github.com>
2024-03-04 19:12:44 +0200

    refactor: typed component

--- client/src/Components/AdminProtectedRoutes.tsx
+++ client/src/Components/AdminProtectedRoutes.tsx
@@ -2,12 +2,13 @@ import React, { useContext } from "react"
 import { UserContext } from "../Context/UserContext"
 import { Navigate, Outlet } from "react-router-dom"
 import Loader from "./Loader"
+import { IUserContext } from "../utils/APIResponseTypes"
 
 function AdminProtectedRoutes() {
-    const context = useContext(UserContext)
+    const context = useContext<IUserContext>(UserContext)
     return context.isLoading ? (
         <Loader text={"Ladataan sisältöä"} />
-    ) : context.user.id && context.user.role === "Admin" ? (
+    ) : context?.user?.id && context?.user?.role === "Admin" ? (
         <Outlet />
     ) : (
         <Navigate to="/login" />
